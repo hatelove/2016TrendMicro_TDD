bd4309b4a22551f34e19b399cab6805c84b84ac6
91 <hatelove@users.noreply.github.com>
2016-01-05 01:02:01 +0800

    Update Day1_HW_TestCost.cpp
    這個看起來不太像是測試程式，比較像是 main(), C++ 應該也有 test framework 可以使用，建議可以找一下。

--- Day1_HW_TestCost/Day1_HW_TestCost/Day1_HW_TestCost.cpp
+++ Day1_HW_TestCost/Day1_HW_TestCost/Day1_HW_TestCost.cpp
@@ -6,6 +6,9 @@
 
 int main()
 {
+	//這一段不容易從測試案例中得知需求的 scenario, 而且額外 depend on TestUtility, 應該有機會直接使用 Test Framework 來取代 TestUtility
+	//可以避免 production code 其實是對的，但 testUtility 寫錯的機率跟風險。
+	
 	TestUtility Tester;
 	Tester.ReloadTable();
 
@@ -13,6 +16,9 @@ int main()
 	Tester.GetCostSet(3, TableRet);
 
 	int nRet = -1;
+	//比較集合跟比較字串的意義是不一樣的，這會影響到我們怎麼設計 production code 的 API
+	//所以這邊如果設計成字串，是偷吃步的，是為了測試而測試，強制讓 production code 把需求跟商業邏輯的產出變成一個 string
+	//如果外部呼叫端要使用這個結果，就還得自己 parsing 這個結果轉成 array，這樣是 bad smell, 代表這個方法不好用
 	string TableExceptedRet = "{6}{15}{24}{21}";
 	if (TableRet == TableExceptedRet) {
 		nRet = 0;
